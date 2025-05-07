12acc033f9f460b183d397f9fa3472922245851b
rogger <rogger@gmail.com>
2025-05-07 11:50:31 -0300

    Implementando permissão de ADM na rota de cadastro de quadras

--- routers/quadras.py
+++ routers/quadras.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from services.quadras import create_quadrant_service
 from schemas.quadras import QuadraCreate
-from auth.auth import token_verifier
+from auth.auth import token_verifier, get_current_user_id
 from database.database import get_db
 
 router = APIRouter(
@@ -12,5 +12,5 @@ router = APIRouter(
 )
 
 @router.post("/cadastrar")
-async def register_quadrant(quadra: QuadraCreate, db: Session = Depends(get_db)):
-    return create_quadrant_service(db=db, quadra=quadra)
+async def register_quadrant(quadra: QuadraCreate, db: Session = Depends(get_db), user_id = Depends(get_current_user_id)):
+    return create_quadrant_service(db=db, quadra=quadra, user_id=user_id)
