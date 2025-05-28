b515d4a4570b83d7862b5737e131ec96d66cd305
rogger <rogger@gmail.com>
2025-05-28 10:22:51 -0300

    Alterando a main para a nova rota

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from routers import usuarios, quadras, agendamentos
 app = FastAPI()
 
 app.include_router(usuarios.router)
+app.include_router(usuarios.routerUser)
 app.include_router(quadras.router)
 app.include_router(agendamentos.router)
